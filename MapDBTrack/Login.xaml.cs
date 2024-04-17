636d65c30cd247d4957e9f88d4d399502b24e6d6
Kusieq <konrad.kus@edu.uekat.pl>
2024-04-17 15:44:51 +0200

    Design of login page

---
+++ MapDBTrack/Login.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MapDBTrack
+{
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void SignIn(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SignUp(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckNetwork(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoginChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PasswordChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
