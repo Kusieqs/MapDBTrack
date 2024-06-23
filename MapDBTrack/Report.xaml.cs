7c2544c4827ce0b0cc42d521f3441c87e2157c6c
Kusieq <konrad.kus@edu.uekat.pl>
2024-06-23 22:56:33 +0200

    Creatinf PDF file with customers (report)

--- MapDBTrack/Report.xaml.cs
+++ MapDBTrack/Report.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using Newtonsoft.Json;
+using PdfSharpCore.Drawing;
+using PdfSharpCore.Pdf;
 using System.IO;
+using System.Numerics;
 using System.Text.Json.Serialization;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,24 +12,93 @@ namespace MapDBTrack
 {
     public partial class Report : Window
     {
+        string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
         public Report()
         {
             InitializeComponent();
         }
         private void PdfClick(object sender, RoutedEventArgs e)
         {
+            List<Place> places = ListCreate();
 
-        }
+            PdfDocument pdf = new PdfDocument();
+            PdfPage page = pdf.AddPage();
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+            XFont font = new XFont("Calibri", 12, XFontStyle.Regular);
+            int y = 30;
+            int x = 10;
+            int lineHeight = 24;
+
+            foreach (var item in places)
+            {
+                if(y + lineHeight > page.Height.Point - 25)
+                {
+                    page = pdf.AddPage();
+                    gfx = XGraphics.FromPdfPage(page);
+                    y = 30;
+                }
+
+                for(int i = 0; i < 9; i++)
+                {
+                    string content = "";
+                    switch (i)
+                    {
+                        case 0:
+                            content = $"Id: {item.customer_id}";
+                            break;
+                        case 1:
+                            content = $"First Name: {item.first_name}";
+                            break;
+                        case 2:
+                            content = $"Last Name: {item.last_name}";
+                            break;
+                        case 3:
+                            content = $"Number: {item.contact_number}";
+                            break;
+                        case 4:
+                            content = $"Email: {item.email}";
+                            break;
+                        case 5:
+                            content = $"City: {item.postal_code}";
+                            break;
+                        case 6:
+                            content = $"Postal Code: {item.postal_code}";
+                            break;
+                        case 7:
+                            content = $"Province: {item.province}";
+                            break;
+                        case 8:
+                            content = $"Street: {item.street}";
+                            break;
+                    }
+                    gfx.DrawString(content, font, XBrushes.Black, x, y);
+                    y += lineHeight;
+                }
+                gfx.DrawString(" ", font, XBrushes.Black, x, y);
+                y += lineHeight;
+                gfx.DrawString(" ", font, XBrushes.Black, x, y);
+                y += lineHeight;
+            }
+            pdf.Save(Path.Combine(desktop, "Report_PDF.pdf"));
+        } // Creating PDF file 
         private void ExcelClick(object sender, RoutedEventArgs e)
         {
 
         }
         private void JsonClick(object sender, RoutedEventArgs e)
         {
+            List<Place> places = ListCreate();
+            string json = JsonConvert.SerializeObject(places);
+            File.WriteAllText(Path.Combine(desktop,"Report_Json.json"), json);
+            MessageBox.Show("The file has been saved", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            this.Close();
+        } // Creating json file
+
+        private List<Place> ListCreate()
+        {
             List<Place> places = new List<Place>();
-
-
-            for(int i = 0; i < MainWindow.customer.Children.Count; i++)
+            for (int i = 0; i < MainWindow.customer.Children.Count; i++)
             {
                 Border border = MainWindow.customer.Children[i] as Border;
                 StackPanel stackPanel = border.Child as StackPanel;
@@ -34,16 +106,8 @@ namespace MapDBTrack
                 Place p1 = MainWindow.places.Where(x => x.customer_id == textBlock.Text).FirstOrDefault();
                 places.Add(p1);
             }
-
-
-            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            string json = JsonConvert.SerializeObject(places);
-            File.WriteAllText(Path.Combine(desktop,"Report_Json"), json);
-            MessageBox.Show("The file has been saved", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
-            this.Close();
+            return places;
         }
-
-
         private void ExitClick(object sender, RoutedEventArgs e)
         {
             this.Close();
