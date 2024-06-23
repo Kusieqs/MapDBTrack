2dc3df36d6fbb1726a974a78d27047fe410dc5e5
Kusieq <konrad.kus@edu.uekat.pl>
2024-06-24 00:04:13 +0200

    Creating excel report

--- MapDBTrack/Report.xaml.cs
+++ MapDBTrack/Report.xaml.cs
@@ -7,12 +7,13 @@ using System.Text.Json.Serialization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-
+using OfficeOpenXml;
 namespace MapDBTrack
 {
     public partial class Report : Window
     {
         string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
 
         public Report()
         {
@@ -81,10 +82,53 @@ namespace MapDBTrack
                 y += lineHeight;
             }
             pdf.Save(Path.Combine(desktop, "Report_PDF.pdf"));
+            MessageBox.Show("The file has been saved", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            this.Close();
         } // Creating PDF file 
         private void ExcelClick(object sender, RoutedEventArgs e)
         {
+            List<Place> places = ListCreate();
 
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (var package = new ExcelPackage(new FileInfo(Path.Combine(desktop,"Report_excel.xlsx"))))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Products");
+
+                worksheet.Cells[1, 1].Value = "Id";
+                worksheet.Cells[1, 2].Value = "First name";
+                worksheet.Cells[1, 3].Value = "Last name";
+                worksheet.Cells[1, 4].Value = "Number";
+                worksheet.Cells[1, 5].Value = "Email";
+                worksheet.Cells[1, 6].Value = "City";
+                worksheet.Cells[1, 7].Value = "Postal code";
+                worksheet.Cells[1, 8].Value = "Province";
+                worksheet.Cells[1, 9].Value = "Street";
+
+                var range = worksheet.Cells["A1:I1"];
+                range.Style.Font.Bold = true;
+                range.Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                range.Style.Fill.BackgroundColor.SetColor(OfficeOpenXml.Style.ExcelIndexedColor.Indexed10);
+                range.Style.Border.Bottom.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                range.Style.Border.Bottom.Color.SetColor(OfficeOpenXml.Drawing.eThemeSchemeColor.Accent3);
+
+                for (int i = 0; i < places.Count; i++)
+                {
+                    worksheet.Cells[i + 3, 1].Value = places[i].customer_id;
+                    worksheet.Cells[i + 3, 2].Value = places[i].first_name;
+                    worksheet.Cells[i + 3, 3].Value = places[i].last_name;
+                    worksheet.Cells[i + 3, 4].Value = places[i].contact_number;
+                    worksheet.Cells[i + 3, 5].Value = places[i].email;
+                    worksheet.Cells[i + 3, 6].Value = places[i].city;
+                    worksheet.Cells[i + 3, 7].Value = places[i].postal_code;
+                    worksheet.Cells[i + 3, 8].Value = places[i].province;
+                    worksheet.Cells[i + 3, 9].Value = places[i].street;
+                }
+                worksheet.Cells.AutoFitColumns();
+                package.Save();
+
+            }
+            MessageBox.Show("The file has been saved", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            this.Close();
         }
         private void JsonClick(object sender, RoutedEventArgs e)
         {
@@ -94,7 +138,6 @@ namespace MapDBTrack
             MessageBox.Show("The file has been saved", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
             this.Close();
         } // Creating json file
-
         private List<Place> ListCreate()
         {
             List<Place> places = new List<Place>();
